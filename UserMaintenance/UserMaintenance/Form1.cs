5140ba2b8201e170a27b56ac9494344cffa52024
bernadettlikker <bernadett.likker@stud.uni-corvinus.hu>
2020-10-18 21:41:05 +0200

    export

--- UserMaintenance/UserMaintenance/Form1.cs
+++ UserMaintenance/UserMaintenance/Form1.cs
@@ -52,6 +52,29 @@ namespace UserMaintenance
 
                 // Tábla létrehozása
                 CreateTable(); // Ennek megírása a következő feladatrészben következik
+                string[] headers = new string[] {
+                    "Kód",
+                    "Eladó",
+                    "Oldal",
+                    "Kerület",
+                    "Lift",
+                    "Szobák száma",
+                    "Alapterület (m2)",
+                    "Ár (mFt)",
+                    "Négyzetméter ár (Ft/m2)"};
+
+                for
+                     xlSheet.Cells[1, 1] = headers[0];
+                      object[,] values = new object[Flats.Count, headers.Length];
+
+                int counter = 0;
+                foreach (Flat f in Flats)
+                {
+                    values[counter, 0] = f.Code;
+                    // ...
+                    values[counter, 8] = "";
+                    counter++;
+                }
 
                 // Control átadása a felhasználónak
                 xlApp.Visible = true;
@@ -88,5 +111,26 @@ namespace UserMaintenance
         {
 
         }
+
+        private string GetCell(int x, int y)
+        {
+            string ExcelCoordinate = "";
+            int dividend = y;
+            int modulo;
+
+            while (dividend > 0)
+            {
+                modulo = (dividend - 1) % 26;
+                ExcelCoordinate = Convert.ToChar(65 + modulo).ToString() + ExcelCoordinate;
+                dividend = (int)((dividend - modulo) / 26);
+            }
+            ExcelCoordinate += x.ToString();
+
+            return ExcelCoordinate;
+        }
+
+        xlSheet.get_Range(
+            GetCell(2, 1),
+             GetCell(1 + values.GetLength(0), values.GetLength(1))).Value2 = values;
     }
 }
